5fde393fc233453c84c5a85cb9675916bb4016b8
elORE0 <158122296+elORE0@users.noreply.github.com>
2024-01-29 22:14:36 -0500

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "QUIERES SER MI SAN VALENTIN",
+  descriptionHeader:
+    "TE AMO PRINCESA ESTE SERA NUESTRO SEGUNDO 14 DE FEBRERO JUNTOS",
+  buttonYes: "SI, ACEPTO",
+  buttonNo: "NO,GRACIAS",
+  titleModar: "PARA LA MAS LINDA",
+  descriptionModar:
+    "Este año esta lleno de cambios y tendremos que adaptarnos, debido a esto tenemos mirar como van a funcionar nuestras celebraciones </br> pero esto no va a impedir demostrarte el amor que siento por tu</br> Lov you <3",
+};
